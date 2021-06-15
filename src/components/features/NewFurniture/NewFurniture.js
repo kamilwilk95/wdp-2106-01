827ee7b4e9dca3d342f57f106a912bf6eeb040e4
Maciej Burak <maciej.burak97@onet.pl>
2021-06-15 14:26:34 +0200

    First version of Swipeable component

--- src/components/features/NewFurniture/NewFurniture.js
+++ src/components/features/NewFurniture/NewFurniture.js
@@ -9,8 +9,8 @@ class NewFurniture extends React.Component {
   state = {
     activePage: 0,
     activeCategory: 'bed',
-    activePageStyle: styles.fadeIn,
     index: 0,
+    activePageStyle: styles.fadeIn,
   };
 
   handlePageChange(newPage) {
@@ -26,14 +26,22 @@ class NewFurniture extends React.Component {
       this.setState({ activeCategory: newCategory, activePageStyle: styles.fadeIn });
     }, 1000);
   }
+  splitToChunks(array) {
+    let result = [];
+    const chunk = 8;
+    for (let i = 0 * 8, j = array.length; i < j; i += chunk) {
+      let temparray = array.slice(i, i + chunk);
+      result.push(temparray);
+    }
+    return result;
+  }
 
   render() {
     const { categories, products } = this.props;
-    const { activeCategory, activePage, activePageStyle } = this.state;
-
+    const { activeCategory, activePage } = this.state;
     const categoryProducts = products.filter(item => item.category === activeCategory);
     const pagesCount = Math.ceil(categoryProducts.length / 8);
-
+    this.splitToChunks(categoryProducts);
     const dots = [];
     for (let i = 0; i < pagesCount; i++) {
       dots.push(
@@ -47,9 +55,6 @@ class NewFurniture extends React.Component {
         </li>
       );
     }
-
-    //console.log(categories.includes(Object.values(activeCategory)))
-
     return (
       <div className={styles.root}>
         <div className='container'>
@@ -60,47 +65,38 @@ class NewFurniture extends React.Component {
               </div>
               <div className={'col ' + styles.menu}>
                 <ul>
-                  <Swipeable
-                    className='row'
-                    profiles={categories}
-                    parentCallback={this.handleCategoryChange.bind(this)}
-                    currentValue={activeCategory}
-                  >
-                    {categories.map(item => (
-                      <li key={item.id}>
-                        <a
-                          className={item.id === activeCategory && styles.active}
-                          onClick={() => this.handleCategoryChange(item.id)}
-                        >
-                          {item.name}
-                        </a>
-                      </li>
-                    ))}
-                  </Swipeable>
+                  {categories.map(item => (
+                    <li key={item.id}>
+                      <a
+                        className={item.id === activeCategory && styles.active}
+                        onClick={() => (
+                          this.handleCategoryChange(item.id), this.handlePageChange(0)
+                        )}
+                      >
+                        {item.name}
+                      </a>
+                    </li>
+                  ))}
                 </ul>
-              </div>
-              <div>
-                <Swipeable
-                  params={{
-                    navigation: {
-                      nextEl: '.swiper-button-next',
-                      prevEl: '.swiper-button-prev',
-                    },
-                  }}
-                />
               </div>
               <div className={'col-auto ' + styles.dots}>
                 <ul>{dots}</ul>
               </div>
             </div>
           </div>
-          <div className={'row ' + activePageStyle}>
-            {categoryProducts.slice(activePage * 8, (activePage + 1) * 8).map(item => (
-              <div key={item.id} className='col-3'>
-                <ProductBox {...item} />
-              </div>
-            ))}
-          </div>
+          <Swipeable
+            containerClassName={styles.swipeWrap}
+            currentValue={activePage}
+            parentCallback={this.handlePageChange.bind(this)}
+          >
+            {this.splitToChunks(categoryProducts).map(element =>
+              element.map(product => (
+                <div key={product.id} className='col-3'>
+                  <ProductBox {...product} />
+                </div>
+              ))
+            )}
+          </Swipeable>
         </div>
       </div>
     );
--- src/components/layout/Swipeable/Swipeable.js
+++ src/components/layout/Swipeable/Swipeable.js
@@ -1,79 +1,42 @@
-import React from 'react';
-import styles from './Swipeable.module.scss';
+import React, { useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper-bundle.css';
 import PropTypes from 'prop-types';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  faArrowAltCircleLeft,
-  faArrowAltCircleRight,
-} from '@fortawesome/free-solid-svg-icons';
 
-class Swipeable extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      index: 0,
-      disabledNext: false,
-      disabledPrev: true,
-    };
-    this.z = 0;
-  }
-
-  checkOfValues() {
-    for (let i of this.props.profiles) {
-      if (Object.values(i).includes(this.props.currentValue) === true) {
-        //console.log(this.z);
-      }
-      this.z = this.z + 1;
-    }
-  }
-
-  togglePrev(e) {
-    this.checkOfValues();
-    let index = this.state.index - 1;
-    let disabledPrev = index === 0;
-    this.setState({ index: index, disabledPrev: disabledPrev, disabledNext: false });
-    this.props.parentCallback(this.props.profiles[index].id);
-  }
-
-  toggleNext(e) {
-    this.checkOfValues();
-    let index = this.state.index + 1;
-    let disabledNext = index === this.props.profiles.length - 1;
-    this.setState({ index: index, disabledNext: disabledNext, disabledPrev: false });
-    this.props.parentCallback(this.props.profiles[index].id);
-  }
-
-  render() {
-    const { index, disabledNext, disabledPrev } = this.state;
-    const profile = this.props.profiles ? this.props.profiles[index] : null;
-    if (this.props.currentValue !== undefined) {
-      //console.log(this.props.currentValue)
-    }
-
-    if (profile) {
-      return (
-        <div className={styles.switchContainer}>
-          <button onClick={e => this.togglePrev(e)} disabled={disabledPrev}>
-            <FontAwesomeIcon icon={faArrowAltCircleLeft} />
-          </button>
-          {this.props.children}
-          <button onClick={e => this.toggleNext(e)} disabled={disabledNext}>
-            <FontAwesomeIcon icon={faArrowAltCircleRight} />
-          </button>
-          <h1>{profile.name}</h1>
-        </div>
-      );
-    } else {
-      return <span>error</span>;
-    }
-  }
+function Swipeable(props) {
+  const [index, setIndex] = useState(0);
+  const handleAddValue = () => {
+    const newValue = index + 1;
+    setIndex(newValue);
+    return index + 1;
+  };
+  const handleDelateValue = () => {
+    const newValue = index - 1;
+    setIndex(newValue);
+    return index - 1;
+  };
+  return (
+    <div>
+      <Swiper
+        observer={true}
+        observeParents={true}
+        onSlideNextTransitionEnd={() => props.parentCallback(handleAddValue())}
+        onSlidePrevTransitionEnd={() => props.parentCallback(handleDelateValue())}
+      >
+        {props.children.map(child => (
+          <SwiperSlide key={child.key} className={props.containerClassName}>
+            {child}
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
 }
+
 Swipeable.propTypes = {
   parentCallback: PropTypes.func,
-  profiles: PropTypes.node,
   children: PropTypes.node,
-  currentValue: PropTypes.string,
+  containerClassName: PropTypes.string,
 };
 
 export default Swipeable;
