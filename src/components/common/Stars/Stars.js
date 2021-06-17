1e88af891e14f6d35495d2aa1217a4dc1ee4da28
Maciej Burak <maciej.burak97@onet.pl>
2021-06-17 14:19:00 +0200

    Add Stars component

---
+++ src/components/common/Stars/Stars.js
@@ -0,0 +1,141 @@
+import React, { useState } from 'react';
+import styles from './Stars.module.scss';
+import PropTypes from 'prop-types';
+import '../../../styles/settings.scss';
+import { FaStar, FaRegStar } from 'react-icons/fa';
+
+function Stars(props) {
+  const defaultStarsData = [
+    { id: 1, hover: false },
+    { id: 2, hover: false },
+    { id: 3, hover: false },
+    { id: 4, hover: false },
+    { id: 5, hover: false },
+  ];
+  const [data, setData] = useState(defaultStarsData);
+  const [touch, setTouch] = useState(false);
+  function changeStars(id) {
+    const newStarsData = [...defaultStarsData];
+    const exactsIndexs = defaultStarsData.filter(element => element.id <= id);
+    const arrayWithExactsIndexs = [];
+    exactsIndexs.filter(element => arrayWithExactsIndexs.push(element.id));
+    for (let i of newStarsData) {
+      if (arrayWithExactsIndexs.includes(i.id)) {
+        newStarsData[i.id - 1] = {
+          ...i,
+          hover: true,
+        };
+      }
+    }
+    setData(newStarsData);
+  }
+  function returnToBasicForm() {
+    setData(defaultStarsData);
+  }
+  function touched() {
+    setTouch(true);
+  }
+  function noTouched() {
+    setTouch(false);
+  }
+
+  return (
+    <div
+      className={styles.stars}
+      onMouseEnter={() => touched()}
+      onMouseLeave={() => noTouched()}
+      onDoubleClick={e => (
+        props.delateMyStarsChoice(props.stars, props.id), e.preventDefault()
+      )}
+    >
+      {data.map(i => (
+        <a key={i.id} href='#'>
+          {props.myStarsChoice !== 0 ? (
+            touch ? (
+              i.hover ? (
+                <FaStar
+                  onClick={e => (
+                    props.changeMyStarsChoice(i.id, props.id), e.preventDefault()
+                  )}
+                  onMouseEnter={() => changeStars(i.id)}
+                  onMouseLeave={() => returnToBasicForm()}
+                  className={i.hover ? styles.hover : styles.normal}
+                >
+                  {i.id} stars
+                </FaStar>
+              ) : (
+                <FaRegStar
+                  className={styles.normal}
+                  onMouseEnter={() => changeStars(i.id)}
+                  onMouseLeave={() => returnToBasicForm()}
+                >
+                  {i.id} stars
+                </FaRegStar>
+              )
+            ) : i.id <= props.myStarsChoice ? (
+              <FaStar
+                onClick={e => (
+                  props.changeMyStarsChoice(i.id, props.id), e.preventDefault()
+                )}
+                className={styles.hover}
+              >
+                {i.id} stars
+              </FaStar>
+            ) : (
+              <FaRegStar className={styles.normal}>{i.id} stars</FaRegStar>
+            )
+          ) : touch ? (
+            i.hover ? (
+              <FaStar
+                onClick={e => (
+                  props.changeMyStarsChoice(i.id, props.id), e.preventDefault()
+                )}
+                onMouseEnter={() => changeStars(i.id)}
+                onMouseLeave={() => returnToBasicForm()}
+                className={i.hover ? styles.hover : styles.normal}
+              >
+                {i.id} stars
+              </FaStar>
+            ) : (
+              <FaRegStar
+                className={styles.normal}
+                onMouseEnter={() => changeStars(i.id)}
+                onMouseLeave={() => returnToBasicForm()}
+              >
+                {i.id} stars
+              </FaRegStar>
+            )
+          ) : i.id <= props.stars ? (
+            <FaStar
+              onClick={e => (
+                props.changeMyStarsChoice(i.id, props.id), e.preventDefault()
+              )}
+              className={styles.normal}
+            >
+              {i.id} stars
+            </FaStar>
+          ) : (
+            <FaRegStar
+              onClick={e => (
+                props.changeMyStarsChoice(i.id, props.id), e.preventDefault()
+              )}
+              className={styles.normal}
+            >
+              {i.id} stars
+            </FaRegStar>
+          )}
+        </a>
+      ))}
+    </div>
+  );
+}
+
+Stars.propTypes = {
+  stars: PropTypes.number,
+  delateMyStarsChoice: PropTypes.func,
+  id: PropTypes.string,
+  myStarsChoice: PropTypes.number,
+  changeMyStarsChoice: PropTypes.func,
+};
+
+export default Stars;
--- src/redux/productsRedux.js
+++ src/redux/productsRedux.js
@@ -1,6 +1,7 @@
 /* selectors */
 export const getAll = ({ products }) => products;
 export const getCount = ({ products }) => products.length;
+export const getStarsData = ({ starsData }) => starsData;
 
 export const getNew = ({ products }) =>
   products.filter(item => item.newFurniture === true);
@@ -12,6 +13,12 @@ const createActionName = name => `app/${reducerName}/${name}`;
 // action types
 export const SET_FAVOURITE = createActionName('SET_FAVOURITE');
 export const SET_ADD_TO_COMPARE = createActionName('SET_ADD_TO_COMPARE');
+export const SET_CHANGE_MY_STARS_CHOICE = createActionName(
+  'SET_CHANGE_MY_STARS_CHOICE'
+);
+export const SET_DELATE_MY_STARS_CHOICE = createActionName(
+  'SET_DELATE_MY_STARS_CHOICE'
+);
 
 // action creators
 export const createAction_changeFavouriteValue = (id, payload) => ({
@@ -26,6 +33,17 @@ export const createAction_changeAddToCompareValue = (id, payload) => ({
   type: SET_ADD_TO_COMPARE,
 });
 
+export const createAction_changeMyStarsChoice = (payload, id) => ({
+  payload,
+  id,
+  type: SET_CHANGE_MY_STARS_CHOICE,
+});
+export const createAction_delateMyStarsChoice = (payload, id) => ({
+  payload,
+  id,
+  type: SET_DELATE_MY_STARS_CHOICE,
+});
+
 /* reducer */
 export default function reducer(statePart = [], action = {}) {
   switch (action.type) {
@@ -38,22 +56,39 @@ export default function reducer(statePart = [], action = {}) {
       };
       return newArray;
     }
-    case SET_ADD_TO_COMPARE:
-      {
-        const elementsIndex = statePart.findIndex(element => element.id === action.id);
-        const newArray = [...statePart];
-        newArray[elementsIndex] = {
-          ...newArray[elementsIndex],
-          addToCompare: action.payload,
-        };
-        const e = newArray.filter(element => element.addToCompare === true);
-        if (e.length <= 4) {
-          return newArray;
-        } else if (e.length >= 5) {
-          return statePart;
-        }
+    case SET_ADD_TO_COMPARE: {
+      const elementsIndex = statePart.findIndex(element => element.id === action.id);
+      const newArray = [...statePart];
+      newArray[elementsIndex] = {
+        ...newArray[elementsIndex],
+        addedToCompare: action.payload,
+      };
+      const e = newArray.filter(element => element.addToCompare === true);
+      if (e.length <= 4) {
+        return newArray;
+      } else if (e.length >= 5) {
+        return statePart;
       }
-      break;
+    }
+    case SET_CHANGE_MY_STARS_CHOICE: {
+      const elementsIndex = statePart.findIndex(element => element.id === action.id);
+      const newArray = [...statePart];
+      newArray[elementsIndex] = {
+        ...newArray[elementsIndex],
+        myStarsChoice: action.payload,
+      };
+      return newArray;
+    }
+    case SET_DELATE_MY_STARS_CHOICE: {
+      const elementsIndex = statePart.findIndex(element => element.id === action.id);
+      const newArray = [...statePart];
+      newArray[elementsIndex] = {
+        ...newArray[elementsIndex],
+        stars: action.payload,
+        myStarsChoice: 0,
+      };
+      return newArray;
+    }
     default:
       return statePart;
   }
