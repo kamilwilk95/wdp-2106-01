5e0b7c5f40646c617681fe7b9c0377460975c03e
wojbiel833 <wojbiel833@gmail.com>
2021-06-20 21:00:16 +0200

    WDP210601-39 add Heading

--- src/components/views/ProductPage/ProductPage.js
+++ src/components/views/ProductPage/ProductPage.js
@@ -1,6 +1,9 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
+
 import styles from './ProductPage.module.scss';
+
+import Stars from '../../common/Stars/Stars';
 
 const ProductPage = () => (
   <div className={styles.root}>
@@ -10,12 +13,32 @@ const ProductPage = () => (
           <h3>Furniture</h3>
           <p>Home &gt; Furniture &gt; Chair</p>
         </div>
-        <div className={styles.productDescription}></div>
+        <div className={styles.productDescription}>
+          <div className={styles.image}>
+            <img
+              src='https://i.ibb.co/Kw9mJ44/pexels-martin-p-chy-1866149-1.jpg'
+              alt='product-photo'
+            />
+          </div>
+          <div className={styles.description}>
+            <div className={styles.header}>
+              <h3>Sunday Magic Bear Sofa</h3>
+              <div className={styles.rating}>
+                <Stars />
+                <p>(0 viewers) Add your review</p>
+              </div>
+            </div>
+            <div className={styles.price}>
+              <p>$350.00</p>
+              <p>$250.00</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 );
 
-// ProductPage.propTypes = {};
+ProductPage.propTypes = {};
 
 export default ProductPage;
