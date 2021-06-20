23a8f2716e89aa50ad00cc0d9dd4c2e91d304152
wojbiel833 <wojbiel833@gmail.com>
2021-06-20 09:31:43 +0200

    stars

--- src/components/views/ProductPage/ProductPage.js
+++ src/components/views/ProductPage/ProductPage.js
@@ -2,7 +2,19 @@ import React from 'react';
 // import PropTypes from 'prop-types';
 import styles from './ProductPage.module.scss';
 
-const ProductPage = () => <div className={styles.root}>This is ProductPage</div>;
+const ProductPage = () => (
+  <div className={styles.root}>
+    <div className={styles.grayRectangle}>
+      <div className='container'>
+        <div className={styles.topBar}>
+          <h3>Furniture</h3>
+          <p>Home &gt; Furniture &gt; Chair</p>
+        </div>
+        <div className={styles.productDescription}></div>
+      </div>
+    </div>
+  </div>
+);
 
 // ProductPage.propTypes = {};
 
