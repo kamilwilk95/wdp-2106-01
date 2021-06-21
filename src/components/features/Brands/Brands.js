a4b71ee3043f031559ceb5ab1860da0fb52a40de
julia-kocik <74269572+julia-kocik@users.noreply.github.com>
2021-06-21 13:40:16 +0200

    Fix WDP210601-31

--- src/components/features/Brands/Brands.js
+++ src/components/features/Brands/Brands.js
@@ -7,65 +7,64 @@ import Button from '../../common/Button/Button';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 
-const Brands = ({ brands }) => {
-  const next = e => {
-    e.preventDefault();
-    const brandOne = document.querySelector('.brandOne');
-    const brandTwo = document.querySelector('.brandTwo');
-    brandOne.classList.remove('d-flex');
-    brandOne.classList.add('d-none');
-    brandTwo.classList.remove('d-none');
-    brandTwo.classList.add('d-flex');
+class Brands extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      imgToDisplayDesktop1: 6,
+      imgToDisplayDesktop2: 12,
+      activePage: 0,
+    };
+    this.next = this.next.bind(this);
+    this.prev = this.prev.bind(this);
+  }
+  static propTypes = {
+    brands: PropTypes.array,
   };
-  const prev = e => {
+  next(e) {
     e.preventDefault();
-    const brandOne = document.querySelector('.brandOne');
-    const brandTwo = document.querySelector('.brandTwo');
-    brandOne.classList.remove('d-none');
-    brandOne.classList.add('d-flex');
-    brandTwo.classList.remove('d-flex');
-    brandTwo.classList.add('d-none');
-  };
-  return (
-    <div className={styles.root}>
-      <div className={'container ' + styles.container}>
-        <div className='row'>
-          <div className='col-1 d-flex justify-content-end align-items-center'>
-            <Button variant='small' className={styles.button} onClick={prev}>
-              <FontAwesomeIcon icon={faChevronLeft}></FontAwesomeIcon>
-            </Button>
-          </div>
-          <div className={'col-10 d-flex brandOne ' + styles.imageBox}>
-            {brands.map(brandData =>
-              brandData.id <= brands.length / 2 ? (
-                <img key={brandData.id} src={brandData.src} alt='brands1' />
-              ) : (
-                ''
-              )
-            )}
-          </div>
-          <div className={'col-10 d-none brandTwo ' + styles.imageBox}>
-            {brands.map(brandData =>
-              brandData.id > brands.length / 2 ? (
-                <img key={brandData.id} src={brandData.src} alt='brands2' />
-              ) : (
-                ''
-              )
-            )}
-          </div>
-          <div className='col-1 d-flex align-items-center'>
-            <Button variant='small' className={styles.button} onClick={next}>
-              <FontAwesomeIcon icon={faChevronRight}></FontAwesomeIcon>
-            </Button>
+    if (this.state.activePage === 0) {
+      this.setState({ activePage: this.state.activePage + 1 });
+    }
+  }
+  prev(e) {
+    e.preventDefault();
+    if (this.state.activePage === 1) {
+      this.setState({ activePage: this.state.activePage - 1 });
+    }
+  }
+  render() {
+    const { brands } = this.props;
+    const images1 = brands.slice(0, this.state.imgToDisplayDesktop1);
+    const images2 = brands.slice(6, this.state.imgToDisplayDesktop2);
+    return (
+      <div className={styles.root}>
+        <div className={'container ' + styles.container}>
+          <div className='row'>
+            <div className='col-1 d-flex justify-content-end align-items-center'>
+              <Button variant='small' className={styles.button} onClick={this.prev}>
+                <FontAwesomeIcon icon={faChevronLeft}></FontAwesomeIcon>
+              </Button>
+            </div>
+            <div className={'col-10 d-flex brandOne ' + styles.imageBox}>
+              {this.state.activePage === 0
+                ? images1.map(brandData => (
+                    <img key={brandData.id} src={brandData.src} alt='brands1' />
+                  ))
+                : images2.map(brandData => (
+                    <img key={brandData.id} src={brandData.src} alt='brands2' />
+                  ))}
+            </div>
+            <div className='col-1 d-flex align-items-center'>
+              <Button variant='small' className={styles.button} onClick={this.next}>
+                <FontAwesomeIcon icon={faChevronRight}></FontAwesomeIcon>
+              </Button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-};
-
-Brands.propTypes = {
-  brands: PropTypes.array,
-};
+    );
+  }
+}
 
 export default Brands;
