ed38c5faf2ca83c33b269e3428a5265ce87913d7
julia-kocik <74269572+julia-kocik@users.noreply.github.com>
2021-06-22 15:55:09 +0200

    Add WDP210601-20

--- src/components/layout/MainLayout/MainLayout.js
+++ src/components/layout/MainLayout/MainLayout.js
@@ -7,16 +7,44 @@ import Brands from '../../features/Brands/BrandsContainer';
 import StickyBarContainer from '../StickyBar/StickyBarContainer';
 import LatestBlog from '../../features/LatestBlog/LatestBlogContainer';
 
-const MainLayout = ({ children }) => (
-  <div>
-    <Header />
-    <StickyBarContainer />
-    {children}
-    <LatestBlog />
-    <Brands />
-    <Footer />
-  </div>
-);
+const MainLayout = ({ children }) => {
+  const [dimensions, setDimensions] = React.useState({
+    width: window.innerWidth,
+    mode: '',
+  });
+  React.useEffect(() => {
+    function handleResize() {
+      if (dimensions.width < 768) {
+        setDimensions({
+          width: window.innerWidth,
+          mode: 'mobile',
+        });
+      } else if (dimensions.width >= 768 && dimensions.width < 992) {
+        setDimensions({
+          width: window.innerWidth,
+          mode: 'tablet',
+        });
+      } else if (dimensions.width >= 992) {
+        setDimensions({
+          width: window.innerWidth,
+          mode: 'desktop',
+        });
+      }
+    }
+
+    window.addEventListener('resize', handleResize);
+  });
+  return (
+    <div>
+      <Header />
+      <StickyBarContainer />
+      {children}
+      <LatestBlog />
+      <Brands />
+      <Footer />
+    </div>
+  );
+};
 
 MainLayout.propTypes = {
   children: PropTypes.node,
