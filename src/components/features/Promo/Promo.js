18790dc46b3f5fc44a01104c3bc3ab0616b534a4
julia-kocik <74269572+julia-kocik@users.noreply.github.com>
2021-06-26 10:43:34 +0200

    Add new adjustments

--- src/components/features/Promo/Promo.js
+++ src/components/features/Promo/Promo.js
@@ -21,60 +21,68 @@ const Promo = ({ promo, deals }) => {
     <div className={styles.root}>
       <div className={'container ' + styles.container}>
         <div className='row'>
-          <div className={'col-4 carousel ' + styles.boxes}>
-            <div className={styles.head}>
-              <h5>HOT DEALS</h5>
-            </div>
+          <div className={'col-4 carouselPromo ' + styles.boxes}>
             <Carousel>
-              <img className={styles.dealsImg} src={deals[0].src} alt='promo' />
+              {deals.map(deal => (
+                <div key={deal.id} className={styles.carouselBox}>
+                  <div className={styles.head}>
+                    <h5>HOT DEALS</h5>
+                  </div>
+                  <img className={styles.dealsImg} src={deal.src} alt='promo' />
+                  <Button className={styles.cartButton} noHover variant='small'>
+                    {' '}
+                    <FontAwesomeIcon icon={faShoppingBasket}>
+                      Favorite
+                    </FontAwesomeIcon>{' '}
+                    Add to Cart
+                  </Button>
+                  <div className={styles.divSpan}>
+                    <span>
+                      25 <br />
+                      days
+                    </span>
+                    <span>
+                      10 <br />
+                      hours
+                    </span>
+                    <span>
+                      45 <br />
+                      mins
+                    </span>
+                    <span>
+                      30 <br />
+                      secs
+                    </span>
+                  </div>
+                  <h5 className={styles.bottomTitle}>Aean Ru Bristique</h5>
+                  <div className={styles.starsDiv}>
+                    <Stars />
+                  </div>
+                  <div className={styles.line}></div>
+                  <div className={styles.outlines}>
+                    <Button variant='outline'>
+                      <FontAwesomeIcon icon={faEye}>Favorite</FontAwesomeIcon>
+                    </Button>
+                    <Button variant='outline'>
+                      <FontAwesomeIcon icon={faHeart}>Favorite</FontAwesomeIcon>
+                    </Button>
+                    <Button variant='outline'>
+                      <FontAwesomeIcon icon={faExchangeAlt}>
+                        Add to compare
+                      </FontAwesomeIcon>
+                    </Button>
+                    <div className={styles.price_old}>
+                      <div className={styles.price_old_value}>$ 400.00</div>
+                    </div>
+                    <div className={styles.price}>
+                      <Button noHover variant='small'>
+                        $ 300.00
+                      </Button>
+                    </div>
+                  </div>
+                </div>
+              ))}
             </Carousel>
-            <Button className={styles.cartButton} noHover variant='small'>
-              {' '}
-              <FontAwesomeIcon icon={faShoppingBasket}>Favorite</FontAwesomeIcon> Add to
-              Cart
-            </Button>
-            <div className={styles.divSpan}>
-              <span>
-                25 <br />
-                days
-              </span>
-              <span>
-                10 <br />
-                hours
-              </span>
-              <span>
-                45 <br />
-                mins
-              </span>
-              <span>
-                30 <br />
-                secs
-              </span>
-            </div>
-            <h5 className={styles.bottomTitle}>Aean Ru Bristique</h5>
-            <div className={styles.starsDiv}>
-              <Stars />
-            </div>
-            <div className={styles.line}></div>
-            <div className={styles.outlines}>
-              <Button variant='outline'>
-                <FontAwesomeIcon icon={faEye}>Favorite</FontAwesomeIcon>
-              </Button>
-              <Button variant='outline'>
-                <FontAwesomeIcon icon={faHeart}>Favorite</FontAwesomeIcon>
-              </Button>
-              <Button variant='outline'>
-                <FontAwesomeIcon icon={faExchangeAlt}>Add to compare</FontAwesomeIcon>
-              </Button>
-              <div className={styles.price_old}>
-                <div className={styles.price_old_value}>$ 400.00</div>
-              </div>
-              <div className={styles.price}>
-                <Button noHover variant='small'>
-                  $ 300.00
-                </Button>
-              </div>
-            </div>
           </div>
           <div className={'col-8 ' + styles.boxes}>
             <img src={promo[0].src} alt='promo' />
